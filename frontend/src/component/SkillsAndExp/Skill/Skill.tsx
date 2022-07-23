7e46255a9f3df92bdbfa8bc976049c42f0225d27
Whitelistedd <geforcexxy@gmail.com>
2022-07-23 15:39:27 +0300

    fixed skillsProps wrong import in skill.tsx

--- frontend/src/component/SkillsAndExp/Skill/Skill.tsx
+++ frontend/src/component/SkillsAndExp/Skill/Skill.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 import Image from 'next/image'
-import { SkillProps, skillsType } from '../SkillsAndExp.model'
+import { SkillProps } from '../SkillsAndExp.model'
 
-export const Skill: React.FC<skillsType> = ({ ImageSRC, title, url }) => {
+export const Skill: React.FC<SkillProps> = ({ ImageSRC, title, url }) => {
   return (
     <Anchor href={url}>
       <SkillsImage width={100} height={100} alt={title} src={ImageSRC} />
